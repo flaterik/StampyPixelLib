4babf047c0a22be7d7ac88cfec8d26ad5877e828
erik <erik@gravity.com>
2014-04-24 23:20:35 -0700

    Annnnd, actually working. Input is noisey and probably ugly. But working. Still need to calbrate input 1 aka the blue tape

--- StampyPixelLib.cpp
+++ StampyPixelLib.cpp
@@ -9,10 +9,12 @@
 #include "StampyPixelLib.h"
 #include "math.h"
 
-StampyStrip::StampyStrip(uint16_t pix, uint16_t ledPin, uint16_t inputPin1, uint16_t inputPin2, uint16_t pixCenter) {
+StampyStrip::StampyStrip(uint16_t pix, uint16_t ledPin, uint16_t inputPin1, uint16_t inputPin2, uint16_t pixCenter, int16_t inputZero1, int16_t inputZero2) {
     _strip = new Adafruit_NeoPixel(pix, ledPin, NEO_GRB + NEO_KHZ800);
     _inputPin1 = inputPin1;
     _inputPin2 = inputPin2;
+    _inputZero1 = inputZero1;
+    _inputZero2 = inputZero2;
     _ledPin = ledPin;
     _pix = pix;
     _pixCenter = pixCenter;
@@ -20,6 +22,8 @@ StampyStrip::StampyStrip(uint16_t pix, uint16_t ledPin, uint16_t inputPin1, uint
     _readMin2 = 1024;
     _readMax1 = 0;
     _readMax2 = 0;
+    _xAccel = 0;
+    _yAccel = 0;
 }
 
 void StampyStrip::begin() {
@@ -30,6 +34,125 @@ void StampyStrip::begin() {
 
 uint32_t StampyStrip::getColor(uint8_t r, uint8_t g, uint8_t b) {
     return ((uint32_t)r << 16) | ((uint32_t)g <<  8) | b;
+}
+
+void StampyStrip::loop() {
+    
+    if(_sampleIndex > _sampleBufferSize) {
+        int32_t sampleSum1 = 0;
+        int32_t sampleSum2 = 0;
+        for(int i = 0 ; i < _sampleBufferSize ; i++) {
+            sampleSum1 += _sampleBuffer1[i];
+            sampleSum2 += _sampleBuffer2[i];
+            _sampleBuffer1[i] = 0;
+            _sampleBuffer2[i] = 0;
+        }
+        int32_t averaged1 = (sampleSum1 / _sampleBufferSize) - _inputZero1;
+        int32_t averaged2 = (sampleSum2 / _sampleBufferSize) - _inputZero2;
+        
+        if(averaged1 < _readMin1) {
+            _readMin1 = averaged1;
+            //printRanges();
+        }
+        if(averaged1 > _readMax1) {
+            _readMax1 = averaged1;
+            //printRanges();
+        }
+        if(averaged2 < _readMin2) {
+            _readMin2 = averaged2;
+            //printRanges();
+        }
+        if(averaged2 > _readMax2) {
+            _readMax2 = averaged2;
+            //printRanges();
+        }
+        _xAccel = map(averaged1, _readMin1, _readMax1, -1000, 1000);
+        _yAccel = map(averaged2, _readMin2, _readMax2, -1000, 1000);
+//        Serial.print(_xAccel);
+//        Serial.print(" x:bright ");
+//        Serial.println(getBrightness(0));
+        _sampleIndex = 0;
+        show();
+    }
+
+    int inputRead1 = analogRead(_inputPin1);
+    int inputRead2 = analogRead(_inputPin2);
+    
+    _sampleBuffer1[_sampleIndex] = (uint16_t) inputRead1;
+    _sampleBuffer2[_sampleIndex] = (uint16_t) inputRead2;
+    _sampleIndex++;
+        
+    _loopCount++;
+    
+    if(_loopCount > _loopInterval) _loopCount = 0;
+}
+
+void StampyStrip::show() {
+    //Serial.println(index);
+    for(uint16_t i=0; i < _pix; i++) {
+        uint32_t thisColor = getLightColor(i);
+        _strip->setPixelColor(i, thisColor);
+    }
+    _strip->show();
+}
+
+void StampyStrip::printRanges() {
+    Serial.print(_readMin1);
+    Serial.print(" - ");
+    Serial.print(_readMax1);
+    Serial.print(" & ");
+    Serial.print(_readMin2);
+    Serial.print(" - ");
+    Serial.println(_readMax2);
+}
+
+double StampyStrip::getAngle(uint32_t input1, uint32_t input2) {
+    return atan2((double)input1, (double)input2);
+}
+
+uint32_t StampyStrip::getLightColor(int lightIndex) {
+    int lightOffset = ((float)lightIndex / (float)_pix) * (float)_loopInterval;
+
+    int colorLoopIndex = (_loopCount + lightOffset) % _loopInterval;
+    //Serial.print("using color loop index ");Serial.print(colorLoopIndex);Serial.print(" for light " ); Serial.println(lightIndex);
+    uint32_t color = getLoopedColor(colorLoopIndex, _loopInterval);
+    return color;
+}
+
+float StampyStrip::getBrightness(int index) {
+    return dmap(abs(_xAccel), 0.0, 1000.0, 0.30, 1);
+}
+
+uint8_t StampyStrip::getScaled(uint8_t num, float by) {
+    return (uint8_t)((float)num * by);
+}
+
+uint32_t StampyStrip::getLoopedColor(int index, int interval)
+{
+    float rIndex = (float)(index % interval);
+    float gIndex = (float)((index + (interval/3)) % interval);
+    float bIndex = (float)((index + (2 * (interval/3))) % interval);
+    
+    float rRads = getRadians(rIndex, interval);
+    float gRads = getRadians(gIndex, interval);
+    float bRads = getRadians(bIndex, interval);
+
+    float brightness = getBrightness(index);
+   // Serial.println(brightness);
+    uint8_t r = getScaled(sinMap(rRads), brightness);
+    uint8_t g = getScaled(sinMap(gRads), brightness);
+    uint8_t b = getScaled(sinMap(bRads), brightness);
+                          
+//    Serial.print("index :");
+//    Serial.print(index);
+//    Serial.print(" r: ");
+//    Serial.print(r);
+//    Serial.print(" g: ");
+//    Serial.print(g);
+//    Serial.print(" b: ");
+//    Serial.println(b);
+    //return getColor(r,g,b);
+    return _strip->Color(r, g, b);
 }
 
 void StampyStrip::colorWipeDown(uint32_t c, uint8_t wait) {
@@ -68,150 +191,17 @@ void StampyStrip::rainbowWipeUp(uint8_t wait) {
         _strip->setPixelColor(i, getLoopedColor(i, _pix));
         _strip->setPixelColor(j, getLoopedColor(j, _pix));
         _strip->show();
-        delay(wait);    
+        delay(wait);
     }
-}
-
-void StampyStrip::loop() {
-    
-    int16_t currentIndex;
-    double currentAngle;
-
-    if(_sampleIndex >= _sampleBufferSize - 1) {
-        uint32_t sampleSum1 = 0;
-        uint32_t sampleSum2 = 0;
-        for(int i = 0 ; i < _sampleBufferSize ; i++) {
-            sampleSum1 += _sampleBuffer1[i];
-            sampleSum2 += _sampleBuffer2[i];
-            _sampleBuffer1[i] = 0;
-            _sampleBuffer2[i] = 0;
-        }
-        uint32_t averaged1 = sampleSum1 / _sampleBufferSize;
-        uint32_t averaged2 = sampleSum2 / _sampleBufferSize;
-        if(averaged1 < _readMin1) {
-            _readMin1 = averaged1;
-            printRanges();
-        }
-        if(averaged1 > _readMax1) {
-            _readMax1 = averaged1;
-            printRanges();
-        }
-        if(averaged2 < _readMin2) {
-            _readMin2 = averaged2;
-            printRanges();
-        }
-        if(averaged2 > _readMax2) {
-            _readMax2 = averaged2;
-            printRanges();
-        }
-        long xScaled = map(averaged1, _readMin1, _readMax1, -1000, 1000);
-        long yScaled = map(averaged2, _readMin2, _readMax2, -1000, 1000);
-        float xAccel = xScaled / 1000.0;
-        float yAccel = yScaled / 1000.0;
-        Serial.print(xAccel);
-        Serial.print(" x:y ");
-        Serial.println(yAccel);
-        
-        _lastIndex = currentIndex;
-        currentAngle = getAngle(averaged1, averaged2);
-        if(currentAngle > _maxAngle) {
-            _maxAngle = currentAngle;
-        }
-        _lastAngle = currentAngle;
-        //Serial.println(currentAngle);
-        _sampleIndex = 0;
-        //showPosition(currentIndex, currentSpeed);
-    }
-    else {
-        currentIndex = _lastIndex;
-        //currentSpeed = _lastSpeed;
-    }
-
-    int inputRead1 = analogRead(_inputPin1);
-    int inputRead2 = analogRead(_inputPin2);
-//
-//    Serial.print(inputRead1);
-//    Serial.print(" : ")
-//    Serial.print(inputRead2)
-//    if(inputRead1 < _readLimitLow) inputRead1 = _readLimitLow1;
-//    if(inputRead1 > _readLimitHigh) inputRead1 = _readLimitHigh1;
-    
-    _sampleBuffer1[_sampleIndex++] = (uint16_t) inputRead1;
-    _sampleBuffer2[_sampleIndex++] = (uint16_t) inputRead2;
-    _sampleIndex++;
-        
-    _loopCount++;
-    
-    if(_loopCount > _loopInterval) _loopCount = 0;
-}
-
-//void StampyStrip::showPosition(int index, int16_t currentSpeed) {
-//    //Serial.println(index);
-//    for(uint16_t i=0; i < _pix; i++) {
-//       // Serial.println("settings a pixel color");
-//        _strip->setPixelColor(i, getLightColor(i));
-//        //_strip->show();
-//    }
-//    
-//    _strip->show();
-//
-//    uint8_t red = 255;
-//    uint8_t blue = 255;
-//    uint8_t green = 255;
-//
-//    uint32_t color = getColor(red, green, blue);
-//
-//    ///Serial.print("display color is "); Serial.println(color);
-//    
-//    for(int j=index - (_displaySize/2); j < index + (_displaySize/2); j++) {
-//        if(j > 0 && j < _pix)
-//            _strip->setPixelColor(j, color);
-//    }
-//
-//    _strip->show();
-//}
-
-void StampyStrip::printRanges() {
-    Serial.print(_readMin1);
-    Serial.print(" - ");
-    Serial.print(_readMax1);
-    Serial.print(" & ");
-    Serial.print(_readMin2);
-    Serial.print(" - ");
-    Serial.println(_readMax2);
-}
-
-double StampyStrip::getAngle(uint32_t input1, uint32_t input2) {
-    return atan2((double)input1, (double)input2);
-}
-
-uint32_t StampyStrip::getLightColor(int lightIndex) {
-    int lightOffset = ((float)lightIndex / (float)_pix) * (float)_loopInterval;
-
-    int colorLoopIndex = (_loopCount + lightOffset) % _loopInterval;
-    //Serial.print("using color loop index ");Serial.print(colorLoopIndex);Serial.print(" for light " ); Serial.println(lightIndex);
-    return getLoopedColor(colorLoopIndex, _loopInterval);
-}
-
-uint32_t StampyStrip::getLoopedColor(int index, int interval)
-{
-    float rIndex = (float)(index % interval);
-    float gIndex = (float)((index + (interval/3)) % interval);
-    float bIndex = (float)((index + (2 * (interval/3))) % interval);
-    
-    float rRads = getRadians(rIndex, interval);
-    float gRads = getRadians(gIndex, interval);
-    float bRads = getRadians(bIndex, interval);
-    
-    uint8_t r = sinMap(rRads);
-    uint8_t g = sinMap(gRads);
-    uint8_t b = sinMap(bRads);
-    
-    return getColor(r,g,b);
 }
 
 /* here be math helpers */
 float StampyStrip::fmap(float x, float in_min, float in_max, float out_min, float out_max)
+{
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+double StampyStrip::dmap(double x, double in_min, double in_max, double out_min, double out_max)
 {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
@@ -221,5 +211,6 @@ float StampyStrip::getRadians(float index, int interval) {
 }
 
 uint8_t StampyStrip::sinMap(float rads) {
+    
     return (uint8_t)fmap(sin(rads), -1, 1, _bgMinBrightness, _bgMaxBrightness);
 }
--- StampyPixelLib.h
+++ StampyPixelLib.h
@@ -5,7 +5,7 @@
 
 class StampyStrip {
 public:
-    StampyStrip(uint16_t pix, uint16_t ledPin, uint16_t inputPin1, uint16_t inputPin2, uint16_t pixCenter);
+    StampyStrip(uint16_t pix, uint16_t ledPin, uint16_t inputPin1, uint16_t inputPin2, uint16_t pixCenter,int16_t inputZero1, int16_t inputZero2);
     void loop();
     void rainbowWipeUp(uint8_t wait);
     void rainbowWipeDown(uint8_t wait);
@@ -14,13 +14,12 @@ public:
     uint32_t getColor(uint8_t r, uint8_t g, uint8_t b);
     void begin();
 private:
-    static const uint16_t _loopInterval = 1000;
+    static const uint16_t _loopInterval = 10000;
     //static const uint8_t _displaySize = 4;
     
     static const uint8_t _bgMinBrightness = 0;
-    static const uint8_t _bgMaxBrightness = 200;
-    
-    static const uint8_t _sampleBufferSize = 50;
+    static const uint8_t _bgMaxBrightness = 255;
+    static const uint8_t _sampleBufferSize = 16;
 //    static const int _readLimitHigh1 = 512; //accelerometers are 5v nominal, getting 3.3 without a reference, need to scale to account
 //    static const int _readLimitLow1 = 325;
 //    
@@ -36,30 +35,36 @@ private:
     uint16_t _loopCount;
     
     uint16_t _inputPin1;
-    uint32_t _inputPin2;
-    uint16_t _readMax1;
-    uint16_t _readMin1;
-    uint16_t _readMax2;
-    uint16_t _readMin2;
-    uint8_t _lastIndex;
-    
+    uint16_t _inputPin2;
+    int16_t _inputZero1;
+    int16_t _inputZero2;
+    int16_t _readMax1;
+    int16_t _readMin1;
+    int16_t _readMax2;
+    int16_t _readMin2;
+    double _xAccel;
+    double _yAccel;
     double _maxAngle;
     double _lastAngle;
     
-    uint16_t _sampleBuffer1[_sampleBufferSize];
-    uint16_t _sampleBuffer2[_sampleBufferSize];
+    int16_t _sampleBuffer1[_sampleBufferSize];
+    int16_t _sampleBuffer2[_sampleBufferSize];
     uint8_t _sampleIndex;
     
     uint32_t _bgColor;
     void printRanges();
     float fmap(float x, float in_min, float in_max, float out_min, float out_max);
+    double dmap(double x, double in_min, double in_max, double out_min, double out_max);
+
     float getRadians(float index, int interval);
     uint8_t sinMap(float rads);
     uint32_t getLoopedColor(int index, int interval);
     double getAngle(uint32_t input1, uint32_t input2);
-    //void showPosition(int index, int16_t currentSpeed);
+    void showPosition();
     uint32_t getLightColor(int lightIndex);
-    
+    float getBrightness(int index);
+    uint8_t getScaled(uint8_t num, float by);
+    void show();
 };
 
 
